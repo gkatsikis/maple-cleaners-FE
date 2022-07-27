b1ffd20970d5ad62bd4b1846ce0541abdf078f3b
gkatsikis <geo.bjj.kat@gmail.com>
2022-07-27 19:00:12 -0400

    filter orders by customer

--- src/components/PickupHistory/PickupHistory.jsx
+++ src/components/PickupHistory/PickupHistory.jsx
@@ -1,16 +1,20 @@
 import './PickupHistory.css'
 
 const PickupHistory = ({ profile, orders }) => {
+
+  let myOrders = orders.filter(order => order.profile_id===profile.id)
+  console.log('personal orders', myOrders)
   return ( 
     <div className="shell">
       <h2>Your Pickup History</h2>
-        {orders.map((order) => (
-          <div className="singleOrderCard">
+        {myOrders.map((order) => (
+          <div className="singleOrderCard" key={order.id}>
             <p>{order.date}</p>
             <p>{order.comments}</p>
             <p>
-              {order.cost !==0 ? order.cost : 'pending'}
+              {order.cost !==0 ? order.cost : null}
             </p>
+            <p>{order.status}</p>
           </div>
         ))}
     </div>
